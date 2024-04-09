d96be055cb9877550dc735c48c160253a676eb3d
Ricardo Guerra <RikkuG491@gmail.com>
2024-04-09 09:16:46 -0400

    Added import for validates and deleted migration files and db file

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 # contains definitions of tables and associated schema constructs
 metadata = MetaData(naming_convention={
